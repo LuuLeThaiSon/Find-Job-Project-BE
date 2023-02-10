4c0c41397d72cc2ce1f3668d1456de73ffa46db9
LeQuangTung <lequangtung1410@gmail.com>
2023-02-10 14:14:34 +0700

    fix CategoryRepository

--- src/main/java/com/example/findjobbe/repository/CategoryRepository.java
+++ src/main/java/com/example/findjobbe/repository/CategoryRepository.java
@@ -1,7 +1,9 @@
 package com.example.findjobbe.repository;
 
+import com.example.findjobbe.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
