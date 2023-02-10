31b38ccdda001476bc9df4108f514f299876ecc8
LeQuangTung <lequangtung1410@gmail.com>
2023-02-10 09:50:49 +0700

    crud candidate

---
+++ src/main/java/com/example/findjobbe/repository/CandidateRepository.java
@@ -0,0 +1,9 @@
+package com.example.findjobbe.repository;
+
+import com.example.findjobbe.model.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+}
