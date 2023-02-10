4a040f19cff80063a248b2d143daf004df6fcc66
LeQuangTung <lequangtung1410@gmail.com>
2023-02-10 11:10:00 +0700

    Revert "crud job"
    This reverts commit 7e50ac1436698b78f80721f55213c78e990e9508.

--- src/main/java/com/example/findjobbe/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.findjobbe.repository;
-
-import com.example.findjobbe.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
--- src/main/java/com/example/findjobbe/service/IJobService.java
+++
@@ -1,6 +0,0 @@
-package com.example.findjobbe.service;
-
-import com.example.findjobbe.model.Job;
-
-public interface IJobService extends ICoreCrud<Job, Long>{
-}
