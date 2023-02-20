8011d37b9fb7357993d45063c27927ce509834f4
LuuLeThaiSon <llts28497@gmail.com>
2023-02-20 15:54:22 +0700

    fix query

--- src/main/java/com/example/findjobbe/repository/CompanyRepository.java
+++ src/main/java/com/example/findjobbe/repository/CompanyRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     @Query(value = "select *,sum(quantity) from company c join job j on c.id = j.company_id " +
-            "group by c.name order by sum(quantity) desc",nativeQuery = true)
+            "group by c.id order by sum(quantity) desc",nativeQuery = true)
     List<Company> findTopCompaniesWithHighRecruitmentDemand();
 
     @Query(value = "select count(j) from Job j join Company c on j.company.id = c.id where j.company.id = ?1")
